95c93922b25b1b98f1ecee8dd395e2de1e3a8565
Suresh Potluri <spotlur@gmail.com>
2026-03-16 01:54:43 -0400

    basic  PDFParse helloworld

---
+++ node/pdfparse/pdfparser.js
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import { PDFParse } from 'pdf-parse';
+
+// Load your PDF file
+const pdfBuffer = fs.readFileSync('./ARCHITECTURE-api-flow.pdf');
+
+async function parsePDF1()) {
+    const parser = new PDFParse({ data: pdfBuffer });
+
+    try {
+        // Get text content
+        const textResult = await parser.getText();
+        console.log("Text content:", textResult.text);
+
+        // Get metadata
+        const infoResult = await parser.getInfo();
+        console.log("Number of pages:", infoResult.total);
+        console.log("Info:", infoResult.info);
+        console.log("Version:", infoResult.version);
+    } catch (err) {
+        console.error("Error parsing PDF:", err);
+    } finally {
+        await parser.destroy();
+    }
+}
+
+parsePDF1();
