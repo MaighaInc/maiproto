b01d82c93bbf75a93f0b28dea062edf8e98b8858
Suresh Potluri <spotlur@gmail.com>
2026-03-21 18:42:57 -0400

    Firebase Auth files

--- node/hworlds/06_fbauth/frontend/src/firebase.js
+++ node/hworlds/06_fbauth/frontend/src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+/*
   const firebaseConfig = {
     apiKey: import.meta.env.MAIFB_API_KEY,
     authDomain: import.meta.env.MAIFB_AUTH_DOMAIN,
@@ -9,6 +10,17 @@ import { getAuth } from "firebase/auth";
     messagingSenderId: import.meta.env.MAIFB_MESSAGING_SENDER_ID,
     appId: import.meta.env.MAIFB_APP_ID,
     measurementId: import.meta.env.MAIFB_MEASUREMENT_ID
+  };
+*/
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBt4yerjmCakhje0csoqhtHv_swelUF2go",
+    authDomain: "maiproto.firebaseapp.com",
+    projectId: "maiproto",
+    storageBucket: "maiproto.firebasestorage.app",
+    messagingSenderId: "253858038734",
+    appId: "1:253858038734:web:ba58b25be43e9ae317e19f",
+    measurementId: "G-7BEKLGHCCS"
   };
 
 const app = initializeApp(firebaseConfig);
